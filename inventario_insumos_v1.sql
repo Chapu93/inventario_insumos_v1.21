66fde36b8c33505c26010a29fdc2217b48b161b6
Chapu93 <diegomaza.2013@gmail.com>
2025-11-03 09:22:22 -0300

    Base de datos actualizada 2

--- inventario_insumos_v1.sql
+++ inventario_insumos_v1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 03-11-2025 a las 13:05:24
+-- Tiempo de generación: 03-11-2025 a las 13:21:06
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
